bd6d294b5a96c2e09af04b8d1af84eabf71d5ed2
marclnomaia <maronline35@gmail.com>
2022-11-14 16:07:58 -0500

    commit_11_14

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 function AddNewTask() {
   var list = document.getElementById("list");
-  var text = document.getElementById("task").value;
+  var text = document.getElementById("task_name").value;
+
+  if (text.length === 0) {
+    alert('task need to has at least 01 character');
+    return;
+  }
   var listItem = document.createElement("li");
+
   const textElement = document.createTextNode(text);
-  list.appendChild(textElement);
-  listItem.appendChild(listItem);
+  listItem.appendChild(textElement);
+  list.appendChild(listItem);
+  listItem.className = "list-task";
 }
